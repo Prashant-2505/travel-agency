92ee619b74cea72bf2730628dc5bba9f215e5d1e
Prashant-2505 <sharmaprashant2505@gmail.com>
2022-12-26 13:13:31 +0530

    dynamic navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,24 +4,40 @@ import { Menuitem } from './MenuItem'
 import './Navbar.css'
 
 export default class Navbar extends Component {
+    // here we declare a state intialy false; means its intial show menu sahrp 
+    state = {clicked: false}
+
+    // in handleclick event we set the state
+    handelclick = ()=>
+    {
+        this.setState({clicked: !this.state.clicked})
+    }
     render() {
         return (
             <nav className='NavbarItems'>
-                <h1 className='navbar-logo'>TRAVELLERS</h1>
+                <h1 className='navbar-logo'>TRAVELERS</h1>
 
-                <ul className="nav-menu">
+                <div className="menu-icon" onClick={this.handelclick}>
+                {/* its a if else condition if we clicked if close sahrp icon is there then show menu sharp else menu sharp icon is there then show close sharp */}
+                {/* if we clicked then it check currently stae is waht ,, it is false means it will else condtion icon */}
+                    <ion-icon name={this.state.clicked?"close-sharp":"menu-sharp"}></ion-icon> 
+                </div>
+
+                <ul className={this.state.clicked?"nav-menu active":"nav-menu"}>
                     {Menuitem.map((item, index) => {
                         return (
-                            <li key ={index}>
-                                <a className={item.cName} href={item.url}><ion-icon  name={item.icon}></ion-icon>{item.title}</a>
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}><ion-icon name={item.icon}></ion-icon>{item.title}</a>
                             </li>
                         )
                     })}
 
                     <button>Sign-up</button>
                 </ul>
-          
+
             </nav>
         )
     }
 }
+
+// close-sharp
